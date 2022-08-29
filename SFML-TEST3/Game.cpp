64df1f163ee884359af950e3a249549d8181e6a8
agome <agome@DESKTOP-81ER52L>
2022-08-29 11:48:21 -0300

    Collider Enemies and Bulltes

--- SFML-TEST3/Game.cpp
+++ SFML-TEST3/Game.cpp
@@ -10,18 +10,27 @@ void Game::initWindow()
 
 void Game::initPlayer()
 {
-	_player = new Player;
+	_player = new Player();
+	
+}
+
+Enemy* Game::initEnemies()
+{
+	return new Enemy(rand() % _window -> getSize().x, -100.f);
 }
 
 Bullet* Game::initBullet()
 {
-	return new Bullet(_player->getPosition().x + _player -> getBounds().width / 2, _player->getPosition().y, 0.f, -1.f);
+	return new Bullet(_player->getPosition().x + _player -> getBounds().width / 2.f - 10.f, _player->getPosition().y, 0.f, -1.f);
 }
 
 Game::Game()
 {
 	initWindow();
 	initPlayer();
+	
+	_spawn_timer_max = 20.f;
+	_spawn_timer = _spawn_timer_max;
 }
 
 Game::~Game()
@@ -33,6 +42,11 @@ Game::~Game()
 	for (auto *i : _bullets)
 	{
 		delete i;
+	}
+
+	for (auto enemy : _enemies)
+	{
+		delete enemy;
 	}
 }
 
@@ -105,6 +119,48 @@ void Game::update()
 		counter++;
 	}
 
+	//Enemies
+	_spawn_timer += 0.5f;
+
+	if (_spawn_timer >= _spawn_timer_max)
+	{
+		_enemies.push_back(initEnemies());
+		_spawn_timer = 0.f;
+	}
+
+	for (int i = 0; i < _enemies.size(); i++)
+	{
+		_enemies[i]->update();
+
+		bool enemy_removed = false;
+
+		// bullet collide with enemies
+		for (int j = 0; j < _bullets.size() && !enemy_removed; j++)
+		{
+			if (_bullets[j]->getBounds().intersects(_enemies[i]->getBoundsEnemies()))
+			{
+				// TODO: SACAR VIDA A LOS ENEMIGOS
+				//IF VIDA ENEMIGO = 0
+				delete _enemies[i];
+				_enemies.erase(_enemies.begin() + i);
+
+				delete _bullets[j];
+				_bullets.erase(_bullets.begin() + j);
+
+				enemy_removed = true;
+			}
+		}
+
+		// enemies step out screen
+		if (_enemies[i]->getBoundsEnemies().top > _window -> getSize().y && !enemy_removed)
+		{
+			delete _enemies[i];
+			_enemies.erase(_enemies.begin() + i);
+			std::cout << "Erase: " << _enemies.size() << std::endl;
+		}
+
+	}
+
 }
 
 void Game::render()
@@ -116,9 +172,15 @@ void Game::render()
 	_player->render(*_window);
 
 	//Bullets
-	for (auto *bullet : _bullets)
+	for (auto* bullet : _bullets)
 	{
 		bullet ->render(*_window);
+	}
+	
+	// Enemies
+	for (auto* Enemy : _enemies)
+	{
+		Enemy->render(*_window);
 	}
 
 	//Display
--- SFML-TEST3/Game.h
+++ SFML-TEST3/Game.h
@@ -2,6 +2,7 @@
 #include <SFML/Graphics.hpp>
 #include "Player.h"
 #include "Bullet.h"
+#include "Enemy.h"
 #include<map>
 
 class Game
@@ -10,12 +11,17 @@ private:
 	//Objects
 	sf::RenderWindow* _window;
 	Player* _player;
+	std::vector<Enemy*> _enemies;
 	std::vector<Bullet*> _bullets;
+
+	float _spawn_timer;
+	float _spawn_timer_max;
 
 	// Private Functions
 	void initWindow();
 	void initPlayer();
 	Bullet* initBullet();
+	Enemy* initEnemies();
 
 	
 public:
